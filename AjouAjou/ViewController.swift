a970bef013b5357171abb93cb793a5fd825414e2
Kiwon Jang <nowik93@icloud.com>
2018-02-15 01:38:50 +0900

    Initial Commit

---
+++ AjouAjou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AjouAjou
+//
+//  Created by jang on 2018. 2. 15..
+//  Copyright © 2018년 jang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
